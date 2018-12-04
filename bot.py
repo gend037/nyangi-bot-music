a26abe696b6fa7607893f73f200a6d1d90dd8903
Geno37 <39951609+Geno37@users.noreply.github.com>
2018-12-04 19:55:27 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ ser=[]
 @bot.event
 async def on_ready():
        print('시작!')
-       await bot.change_presence(game=discord.Game(name='보석보다도 아름다운 흰둥이와 놀아주는 중이니라'))
+       await bot.change_presence(game=discord.Game(name='보석보다 아름다운 흰둥이와 놀아주는 중이니라'))
 
 @bot.event
 async def on_message(message):
