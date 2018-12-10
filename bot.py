cf928463c26bc93be766e6ea9dc9041073f9a54e
Geno37 <39951609+Geno37@users.noreply.github.com>
2018-12-10 20:34:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,7 +96,7 @@ async def 놀아줘():
    
 filter = ['cyka blyat', 'fuck', 'ёб твою мать', '씨발', '개새끼', '닥쳐']
 
-@bot.event 
+@bot.listen()
 async def on_message(message):
     if message.content.lower() in filter:
         await bot.delete_message(message) 
